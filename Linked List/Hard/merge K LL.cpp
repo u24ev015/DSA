2b3a2fe207accc6820a27f4da811bde17e4eb1c9
u24ev015 <u24ev015@eced.svnit.ac.in>
2026-03-25 22:43:53 +0530

    merge K LL.cpp

---
+++ Linked List/Hard/merge K LL.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {   
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i=0;i<lists.size();i++)
+    {
+        ListNode* temp=lists[i];
+        while(temp!=NULL)
+        {
+            pq.push(temp->val);
+            temp=temp->next;
+        }
+    }
+    ListNode* dummy= new ListNode(0);
+    ListNode* tdummy=dummy;
+    while(!pq.empty())
+    {
+        int data=pq.top();
+        ListNode* var=new ListNode(data);
+        tdummy->next=var;
+        tdummy=tdummy->next;
+        pq.pop();
+    }
+
+    return dummy->next;
+        
+    }
+};
