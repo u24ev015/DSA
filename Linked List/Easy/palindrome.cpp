c24bff670e13ae8ea74dac9f1aa2d04c513636ed
u24ev015 <u24ev015@eced.svnit.ac.in>
2026-03-25 22:22:24 +0530

    Create palindrome.cpp

---
+++ Linked List/Easy/palindrome.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+    ListNode* temp=head;
+    stack<int>st;
+
+    while(temp!=NULL)
+    {
+        st.push(temp->val);
+        temp=temp->next;
+    }
+
+    while(head!=NULL)
+    {
+        if(st.top()!=head->val)
+        return false;
+        st.pop();
+        head=head->next;
+     }
+    return true;
+   
+    }
+};
