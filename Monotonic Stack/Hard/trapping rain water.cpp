1ac0a8a50dc5606d63579f128ecdd435ce177cc3
u24ev015 <u24ev015@eced.svnit.ac.in>
2026-03-25 22:48:51 +0530

    Create trapping rain water.cpp

---
+++ Monotonic Stack/Hard/trapping rain water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+      int left = 0, right = height.size() - 1;
+        int leftMax = 0, rightMax = 0, water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = max(leftMax, height[left]);
+                water += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = max(rightMax, height[right]);
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+        return water;   
+    }
+};
