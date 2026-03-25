582a9cb420e0b8474ed7e21f7f60353226449c92
u24ev015 <u24ev015@eced.svnit.ac.in>
2026-03-25 23:07:06 +0530

    Create longest valid parenthesis.cpp

---
+++ String/Hard/longest valid parenthesis.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);   // base index
+
+        int maxi = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+
+                if(st.empty()) {
+                    st.push(i);
+                } else {
+                    maxi = max(maxi, i - st.top());
+                }
+            }
+        }
+        return maxi;
+    }
+};
