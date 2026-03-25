f6a44b2d3712b641566c64655cb1c2c0ea4ff73a
u24ev015 <u24ev015@eced.svnit.ac.in>
2026-03-25 22:23:57 +0530

    Create Binary to LL.cpp

---
+++ Linked List/Easy/Binary to LL.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* reverse(ListNode* &head)
+    {
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+
+        while(curr!=NULL)
+        {
+            ListNode* nextnode=curr->next;
+            curr->next=prev;
+            prev=curr;
+
+            curr=nextnode;   
+        }
+        return prev;
+    }
+    int getDecimalValue(ListNode* head) {
+    head=reverse(head);
+    ListNode* temp=head; 
+    long long num=0;
+    int i=0;
+    while(temp!=NULL)
+    {
+        num+=temp->val*pow(2,i);
+        i++;
+        temp=temp->next;
+    } 
+    return num;  
+    }
+};
